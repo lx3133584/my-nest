61cf5b2381549cc7d58df0b60666f81dce9b95bd
liangxu5 <liangxu5@jd.com>
2023-08-15 22:45:25 +0800

    feat: 十分钟内不再发送短信

--- src/sms/sms.controller.ts
+++ src/sms/sms.controller.ts
@@ -4,10 +4,22 @@ import { SMSService } from './sms.service';
 
 @Controller('sms')
 export class SmsController {
-    constructor(private readonly smsService: SMSService) {}
+  private lastSent: Date | null = null;
+  constructor(private readonly smsService: SMSService) { }
 
   @Get('send')
-  sendMsg() {
-    return this.smsService.sendMsg('15510005512');
+  async sendMsg() {
+    const now = new Date();
+    if (this.lastSent && now.getTime() - this.lastSent.getTime() < 10 * 60 * 1000) {
+      // 如果在过去的10分钟内已经发送过短信，直接返回
+      return;
+    }
+
+    const result = await this.smsService.sendMsg('15510005512');
+
+    // 更新上次发送短信的时间
+    this.lastSent = now;
+
+    return result;
   }
 }
